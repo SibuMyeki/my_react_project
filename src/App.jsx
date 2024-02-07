895202d5abadb8c0c7e3fa8337fd78e1c4e8674b
Sibusisiwe <sibusisiwe.myeki@younglings.africa>
2024-02-07 09:42:28 +0200

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+// app test
+const App = () => {
+  return (
+    <div className='container'>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      
+      <ul className='navbar'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
